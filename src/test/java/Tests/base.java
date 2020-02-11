0d8030bd196c24969c0681c2ad15f031e2ad5113
sashazhitin <sasha.z@bkstg.com>
2020-02-11 17:02:04 +0200

    First appium run on android

--- src/test/java/Tests/base.java
+++ src/test/java/Tests/base.java
@@ -1,15 +1,27 @@
 package Tests;
 
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.AndroidElement;
 import io.appium.java_client.remote.MobileCapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 public class base {
 
-    public static void main (String[] args){
+    public static void main(String[] args) throws MalformedURLException {
+        File appDir = new File("src/main/resources");
+        File app = new File(appDir, "ApiDemos-debug.apk");
 
 
         DesiredCapabilities cap = new DesiredCapabilities();
-        cap.setCapability(MobileCapabilityType.DEVICE_NAME,"Pixel_2_API_28");
-        cap.setCapability(MobileCapabilityType.APP, );
+        cap.setCapability(MobileCapabilityType.DEVICE_NAME, "Pixel_2_API_28");
+        cap.setCapability(MobileCapabilityType.AUTOMATION_NAME, "uiautomator2");
+        cap.setCapability(MobileCapabilityType.APP, app.getAbsolutePath());
+
+        AndroidDriver<AndroidElement> driver = new AndroidDriver<AndroidElement>(new URL("http://127.0.0.1:4723/wd/hub"), cap);
+
     }
 }
